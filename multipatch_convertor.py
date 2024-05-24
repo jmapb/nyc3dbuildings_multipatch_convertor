6df03d4dd896eaff74a4ddac3e1845e1fb51b4f3
jmapb <45634056+jmapb@users.noreply.github.com>
2024-05-24 12:08:53 -0400

    improve polygon filtering and height calculation
     - skip processing of non-horizontal polygons
     - use dictionary to ensure only one polygon (with the highest elevation) is created for any given xy coordinate list

--- multipatch_convertor.py
+++ multipatch_convertor.py
@@ -49,33 +49,50 @@ def multipatch_convertor(geodataframe, z_unit_in='m', relative_h=False, save=Fal
     for feature in gdf_gj['features']:
         # Extract properties of each feature
         properties = feature['properties']
-        # Flatten MultiPolygons into list of Polygons
-        polygon_list = [p for mp in feature['geometry']['coordinates'] for p in mp]
-        
+
         # Define minimum height
         if z_unit_in == 'm':
             min_h = 9000
         else:
             min_h = 30000
         
+        # Initialize empty building parts height dictionary. For each entry, the key will be a tuple of the
+        # sequential coordinate pairs (X and Y only, omitting Z) that define the building part polygon, and
+        # the value will be a list of all the Z coordinates for these coordinates pairs, from which we can
+        # find max and min height. (The multipatch data encodes building part tops and bottoms and seperate
+        # polygons with different Z coordinates. It also includes polygons for the walls of buildings, which
+        # we'll filter out below.)
+        building_parts = {};
+
+        # Loop through multipatch data and populate the building_parts dictionary. Skip any polygons with < 4 points 
+        # (first and last point are the same, so 4 will make a triangle, the smallest valid polygon).
+        # Also skip any polygon that includes multiple differring Z coordinates, since these are vertical
+        # building walls.
+        for multipatch_polygon in feature['geometry']['coordinates']:
+            if len(multipatch_polygon[0]) > 3 and len(set([c[2] for c in multipatch_polygon[0]])) == 1:
+                z_coordinate = multipatch_polygon[0][0][2]
+                min_h = min(min_h, z_coordinate)
+                xy_coordinates = tuple((c[0],c[1]) for c in multipatch_polygon[0])
+                if xy_coordinates in building_parts:
+                    building_parts[xy_coordinates].append(z_coordinate)
+                else:
+                    building_parts[xy_coordinates] = [z_coordinate]        
+        
         # Initiate Polygon list for the feature
         splitted_feature_list = []
         
-        # Iterate through Polygon list
-        for polygon in polygon_list:
+        # Iterate through building_parts dictionary
+        for xy_coords, z_coords in building_parts.items():
+        
             # Create new feature and assign properties to each Polygon
             new_feature = properties.copy()
             
             # Extract new feature height
-            height = polygon[0][2]
-            
-            # Compare height to minimum height and save if smaller
-            if height < min_h:
-                min_h = height
+            height = max(z_coords)
             
             # Assign new feature's height and vertices
             new_feature['height'] = height
-            new_feature['geometry'] = Polygon([vertex[:2] for vertex in polygon])
+            new_feature['geometry'] = Polygon(xy_coords)
 
             # Populate polygon list
             splitted_feature_list.append(new_feature)
